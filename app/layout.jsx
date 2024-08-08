af71ab85288f91a1e4575a259596213cdd44162c
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2024-08-08 03:17:25 -0400

    Removes vercel speed analytics and insights, working deployment

--- app/layout.jsx
+++ app/layout.jsx
@@ -29,17 +29,3 @@ export default function RootLayout({ children }) {
 }
 'use client';
 import { Analytics } from '@vercel/analytics/react';
- 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <Analytics mode={'production'} />;
-      </body>
-    </html>
-  );
-}
