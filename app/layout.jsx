0f22944dc01a2e8391f744e0f4f0b35d10415bbc
Arhaan Keshwani <arhaankeshwani200@gmail.com>
2024-08-08 03:01:35 -0400

    Fixed vercel deployment issue

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,25 +1,9 @@
-import { SpeedInsights } from "@vercel/speed-insights/react"
 import { JetBrains_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import PageTransition from "@/components/PageTransition";
 import StairTransition from "@/components/StairTransition";
 
-import { SpeedInsights } from '@vercel/speed-insights/next';
- 
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <title>Next.js</title>
-      </head>
-      <body>
-        {children}
-        <SpeedInsights />
-      </body>
-    </html>
-  );
-}
 const jetbrainsMono = JetBrains_Mono({ 
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
